abc9317ba9b06577f9f6a902d8d9f958a1ebfce0
anxhuang <41319016+anxhuang@users.noreply.github.com>
2018-08-06 22:27:36 +0800

    update 208-07.py

--- 208-XX/208-07.py
+++ 208-XX/208-07.py
@@ -1,13 +1,23 @@
+#練習自訂例外 Custom Exception
+class OverBudgetException (Exception):
+    e="已經超支了\n"
+    pass
+
+budget=1000
 total=0
 book={}
-while True:
-    item=input("消費項目: ")
-    price=int(input("消費金額: "))
-    if (total+price)<=1000:
-        total+=price
-        book[item]=price
-        print(f"目前消費金額: {total}")
-    else:
-        print("已經超支了\n",book)
-        break
+try:
+    while True:
+        item=input("消費項目: ")
+        price=int(input("消費金額: "))
+        total += price
+        if total <= budget:
+            book[item]=price #等同於book.update({item:price}), 沒有這個key就會新增
+            print(f"目前消費金額: {total}")
+        else:
+            raise OverBudgetException
+            break
+except OverBudgetException:
+    print(OverBudgetException.e,book)
 input("Enter to exit")
+
